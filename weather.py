7bcd5f0872bb14d753438626d66449afdc77a02b
Alexos Core Labs <39374140+alexoslabs2@users.noreply.github.com>
2022-07-27 09:59:44 -0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,15 +1,28 @@
 import requests
 
-api_key = "" #Gererate the API Key in https://openweathermap.org/
 city = input("What is the city?\n")
-url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric"+"&appid="+api_key
 
-request = requests.get(url)
-json = request.json()
+def get_weather_desc_and_temp():
+    api_key = "860afc85c18322e3684a0a44b69a0b81"
+    url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric"+"&appid="+api_key
 
-description = json.get("weather")[0].get("description")
-print("Today's forecast in", city, "is", description)
-temp_min = json.get("main").get("temp_min")
-print("The minimum temperature is", temp_min, "grau Celcius")
-temp_max = json.get("main").get("temp_max")
-print("The maximum temperature is", temp_min, "grau Celcius")
+    request = requests.get(url)
+    json = request.json()
+
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
+
+    return {'description': description,
+            'temp_min': temp_min,
+            'temp_max': temp_max}
+
+
+def main():
+    # Print the results
+    weather_dict = get_weather_desc_and_temp()    
+    print("Today's forecast in", city, "is", weather_dict.get('description'))
+    print("The minimum temperature is", weather_dict.get('temp_min'), "graus Celcius")
+    print("The maximum temperature is", weather_dict.get('temp_max'), "graus Celcius")
+
+main()
