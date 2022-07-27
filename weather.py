42bed9b935e7bd0b061b4ff8a4ebe8211fa14dcf
Alexos Core Labs <39374140+alexoslabs2@users.noreply.github.com>
2022-07-27 09:03:08 -0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "860afc85c18322e3684a0a44b69a0b81"
+api_key = "" #Gererate the API Key in https://openweathermap.org/
 city = input("What is the city?\n")
 url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric"+"&appid="+api_key
 
@@ -10,6 +10,6 @@ json = request.json()
 description = json.get("weather")[0].get("description")
 print("Today's forecast in", city, "is", description)
 temp_min = json.get("main").get("temp_min")
-print("The minimum temperature is", temp_min, "graus Celcius")
+print("The minimum temperature is", temp_min, "grau Celcius")
 temp_max = json.get("main").get("temp_max")
-print("The maximum temperature is", temp_min, "graus Celcius")
+print("The maximum temperature is", temp_min, "grau Celcius")
